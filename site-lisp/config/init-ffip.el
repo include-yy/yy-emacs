bc95e770f6e9fba09aec4152727a4944f1c3d89d
include-yy <969041171@qq.com>
2021-08-05 00:41:51 +0800

    ffip use fd on windows

--- site-lisp/config/init-ffip.el
+++ site-lisp/config/init-ffip.el
@@ -1,7 +1,8 @@
 ;; on windows, use find copied from git's uer\bin, and rename to avoid
 ;; name conflict with dos's command FIND
 (when (eq system-type 'windows-nt)
-  (setq ffip-find-executable (concat yy-emacs-root-dir "/winbin/yyfind")))
+  (setq ffip-use-rust-fd t)
+  (setq ffip-find-executable (concat yy-emacs-root-dir "/winbin/fd.exe")))
 
 ;; set key
 (lazy-load-global-keys
